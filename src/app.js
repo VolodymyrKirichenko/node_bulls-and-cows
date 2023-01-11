f16904920c457a68164aaf9547670436049bfca3
Sergio k <valodya.kirik@gmail.com>
2023-01-11 23:16:02 +0200

    project has been added

--- src/app.js
+++ src/app.js
@@ -1,1 +1,40 @@
 'use strict';
+
+const { generation } = require('./generation');
+const { countBullsAndCows } = require('./countBullsAndCows');
+
+const readline = require('readline');
+
+const terminal = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const generationNumbers = generation();
+
+const CustomGame = () => {
+  terminal.question('Write 4 different digits: ', (answer) => {
+    const trimmedAnswer = answer.split(' ').join('');
+
+    const isFourElements = [...new Set(trimmedAnswer)].length === 4;
+
+    if (!isFourElements || isNaN(trimmedAnswer)) {
+      console.log('Please enter only 4 numbers and numbers only');
+      CustomGame();
+    } else {
+      const {
+        bulls, cows,
+      } = countBullsAndCows(generationNumbers, trimmedAnswer);
+
+      if (bulls < 4) {
+        console.log(`BULLS - ${bulls}, COWS - ${cows}. Try again`);
+        CustomGame();
+      } else {
+        console.log('---You won---');
+        terminal.close();
+      }
+    }
+  });
+};
+
+CustomGame();
